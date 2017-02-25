e5adaaa9e6dfb842009618f0ebde3ea2083ca081
mustumar <mustumar@Ms-MacBook-Pro.local>
2017-02-25 17:02:21 +0000

    Update UI

--- SCRIPT/UI_script.js
+++ SCRIPT/UI_script.js
@@ -33,5 +33,5 @@ $(document).ready(function () {
     });
 });
 $(window).scroll(function () {
-    $(".top").css("opacity", 1 - $(window).scrollTop() / 250); 
+    $("#sectionHome").css("opacity", 1 - $(window).scrollTop() / 1250); 
 });
