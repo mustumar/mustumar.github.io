c2a7ed1b897cb5269cda20b30400a6ffee9267e8
cawpat <callumgibbons@Callums-MacBook-Pro.local>
2017-02-25 14:30:26 +0000

    Added space
    Test

--- SCRIPT/UI_script.js
+++ SCRIPT/UI_script.js
@@ -33,5 +33,5 @@ $(document).ready(function () {
     });
 });
 $(window).scroll(function () {
-    $(".top").css("opacity", 1 - $(window).scrollTop() / 250);
+    $(".top").css("opacity", 1 - $(window).scrollTop() / 250); 
 });
